e19b8877a99843807ed00b8e062a8925c39c320e
Sanket Rajendra Shah <sanketshah812@gmail.com>
2023-04-14 17:58:05 +0200

    Added html writer functions

--- homework_3/html_writer/src/html_writer.cpp
+++ homework_3/html_writer/src/html_writer.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <html_writer.hpp>
-
+#include <string>
 
 void html_writer::OpenDocument()
 {
@@ -23,11 +23,43 @@ void html_writer::AddTitle(const std::string& title)
     std::cout << " <title>" << title << "</title>" << std::endl;
 }
 
+void html_writer::OpenBody()
+{
+    std::cout << "<body>" << std::endl;
+}
+
+void html_writer::CloseBody()
+{
+    std::cout << "</body>" << std::endl;
+}
+
+void html_writer::OpenRow()
+{
+    std::cout << "<row>" << std::endl;
+}
+
+void html_writer::CloseRow()
+{
+    std::cout << "</row>" << std::endl;
+}
+
+void html_writer::AddImage(const std::string& img_path, float score, bool highlight)
+{
+    std::cout <<  "<div class=\"column\">" << std::endl;
+    std::size_t found = img_path.find_last_of("/");
+    std::cout << "<h2>" << img_path.substr(found+1)  << "</h2>" << std::endl;
+    std::cout << "<img src=\"" << img_path <<  "/>" << std::endl;
+    std::cout << "<p>score =" << score << "</p>" << std::endl;
+    std::cout << "</div>" << std::endl;
+
+}
+
 int main()
 {
     html_writer::OpenDocument();
     html_writer::AddTitle("Image Browser");
     html_writer::AddCSSStyle("style.css");
+    html_writer::AddImage("/home/motor-ai/workstation/cpp_homeworks/homework_3/web_app/data/000000.png", 0.75, false);
     html_writer::CloseDocument();
 }
 
